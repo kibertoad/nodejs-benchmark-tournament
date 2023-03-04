14e5f3fc2b5869ea6e238a6f3c191a3b1806f243
Igor Savin <iselwin@gmail.com>
2023-03-04 23:19:15 +0200

    Update results for arg passing

--- args-passing/regenerate.cmd
+++ args-passing/regenerate.cmd
@@ -2,16 +2,22 @@ rem Make sure to run this in Admin account
 rem
 call npm run install-node
 timeout /t 2
-call nvm use 10.23.2
+call nvm use 10.24.1
 timeout /t 2
 call npm run benchmark-all
-call nvm use 12.20.1
+call nvm use 12.22.12
 timeout /t 2
 call npm run benchmark-all
-call nvm use 14.15.4
+call nvm use 14.21.3
 timeout /t 2
 call npm run benchmark-all
-call nvm use 15.8.0
+call nvm use 16.19.1
+timeout /t 2
+call npm run benchmark-all
+call nvm use 18.14.2
+timeout /t 2
+call npm run benchmark-all
+call nvm use 19.7.0
 timeout /t 2
 call npm run benchmark-all
 call npm run combine-results
