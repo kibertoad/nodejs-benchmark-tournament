6615bfe1712797ab839e6d7c0fd3ab43705d22b1
Frazer Smith <frazer.dev@outlook.com>
2023-10-04 16:58:55 +0100

    chore: update node 10 and 12 versions

--- dto/regenerate.cmd
+++ dto/regenerate.cmd
@@ -2,10 +2,10 @@ rem Make sure to run this in Admin account
 rem
 call npm run install-node
 timeout /t 2
-call nvm use 10.23.0
+call nvm use 10.24.1
 timeout /t 2
 call npm run benchmark-all
-call nvm use 12.20.0
+call nvm use 12.22.12
 timeout /t 2
 call npm run benchmark-all
 call nvm use 14.15.3
