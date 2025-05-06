0b71f72bacfa6e087c030f4a0d947d832bfc38ad
Frazer Smith <frazer.dev@icloud.com>
2025-05-06 07:47:57 +0100

    feat(string-matching): add cached regex contestant

--- string-matching/executioner.js
+++ string-matching/executioner.js
@@ -3,17 +3,20 @@ const { getMeasureFn } = require("../common/src/executionUtils");
 const { execute: indexofFn } = require("./contestants/indexof");
 const { execute: includesFn } = require("./contestants/includes");
 const { execute: regexFn } = require("./contestants/regex");
+const { execute: regexCachedFn } = require("./contestants/regexCached");
 const { validateAccuracy } = require("./contestants/common");
 
 const contestants = {
   _indexof: getMeasureFn("indexof", indexofFn),
   _includes: getMeasureFn("includes", includesFn),
   _regex: getMeasureFn("regex", regexFn),
+  _regexcached: getMeasureFn("regexcached", regexCachedFn),
 };
 
 validateAccuracy(indexofFn());
 validateAccuracy(includesFn());
 validateAccuracy(regexFn());
+validateAccuracy(regexCachedFn());
 
 const contestant = resolveContestant(contestants);
 contestant();
