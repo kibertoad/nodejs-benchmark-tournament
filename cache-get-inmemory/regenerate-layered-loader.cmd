d0923e12cf54370ad9ea34add79770d7827eb1db
Frazer Smith <frazer.dev@icloud.com>
2025-05-18 12:49:40 +0100

    chore: add node 24; remove node 16; update results

--- cache-get-inmemory/regenerate-layered-loader.cmd
+++ cache-get-inmemory/regenerate-layered-loader.cmd
@@ -2,16 +2,16 @@ rem Make sure to run this in Admin account
 rem
 call npm run install-node
 timeout /t 2
-call nvm use 16.20.2
-timeout /t 2
-call npm run benchmark-layered-loader
 call nvm use 18.20.8
 timeout /t 2
 call npm run benchmark-layered-loader
-call nvm use 20.19.1
+call nvm use 20.19.2
 timeout /t 2
 call npm run benchmark-layered-loader
-call nvm use 22.15.0
+call nvm use 22.15.1
+timeout /t 2
+call npm run benchmark-layered-loader
+call nvm use 24.0.2
 timeout /t 2
 call npm run benchmark-layered-loader
 call npm run combine-results
